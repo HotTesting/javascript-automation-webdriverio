6a5b69e2cbadc5e4b38d8138233b9ab297909fe9
Oleksandr Khotemskyi <xotabu4@gmail.com>
2019-01-22 19:15:04 +0200

    intermediate

--- features/open.feature
+++ features/open.feature
@@ -3,4 +3,4 @@ Feature: Simple test
 
     Scenario: The attribute "role" of a element should be "note"
         Given I open website
-        
+        Then I log 'this'
--- steps/index.ts
+++ steps/index.ts
@@ -1,5 +1,9 @@
-const { Given } = require("cucumber");
+import { Given } from "cucumber";
 
-Given(/^I open website$/, function() {
-    browser.url('')
+Given('I open website', function() {
+  browser.url("");
+});
+
+Given('I log {string}', function(toLog) {
+  console.log('Logging ', toLog)
 });
